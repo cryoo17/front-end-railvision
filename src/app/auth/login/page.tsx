5a74ab22ef58d81395928df79a0f33a229b6f46d
cryoo17 <tgun8113@gmail.com>
2025-05-06 13:43:10 +0700

    feat: middleware

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,18 +1,23 @@
-"use client";
-
 import AuthLayout from "@/components/layouts/AuthLayout/AuthLayout";
-import Login from "@/components/views/Auth/Login/Login";
-// import { Metadata } from "next";
+import { Suspense } from "react";
+import { Metadata } from "next";
+import dynamic from "next/dynamic";
 
-// export const metadata: Metadata = {
-//   title: "Login",
-//   description: "Login",
-// };
+export const metadata: Metadata = {
+  title: "Login",
+  description: "Login",
+};
+
+const Login = dynamic(() => import("@/components/views/Auth/Login/Login"), {
+  ssr: false,
+});
 
 const LoginPage = () => {
   return (
     <AuthLayout>
-      <Login />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Login />
+      </Suspense>
     </AuthLayout>
   );
 };
