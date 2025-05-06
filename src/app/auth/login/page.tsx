86aa49808949b859e012810d3535d697cf224529
cryoo17 <tgun8113@gmail.com>
2025-05-06 13:29:44 +0700

    feat: middleware

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,13 +1,11 @@
-"use client";
-
 import AuthLayout from "@/components/layouts/AuthLayout/AuthLayout";
 import Login from "@/components/views/Auth/Login/Login";
-// import { Metadata } from "next";
+import { Metadata } from "next";
 
-// export const metadata: Metadata = {
-//   title: "Login",
-//   description: "Login",
-// };
+export const metadata: Metadata = {
+  title: "Login",
+  description: "Login",
+};
 
 const LoginPage = () => {
   return (
